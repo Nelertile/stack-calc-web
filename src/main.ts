793021647a410ca568a02439a3e67768afbf6df6
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2022-03-16 15:49:36 +0100

    better style

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './style/style.css'
 
-const out = document.querySelector('.out')!
-const input = document.querySelector('.input')!
+const out = document.querySelector('#out')!
+const input = document.querySelector('#input')!
 input.addEventListener('change', calculate)
 
 function calculate(){
-  if(!input.value) return out.textContent = "That ain't no items!"
+  if(input.value < 0) input.value = 0
+  if(input.value == 0) return out.textContent = "That ain't no items!"
   if(input.value < 64) return out.textContent = "That's not even a stack..."
   
   const stack: number = Math.ceil(input.value / 64)
